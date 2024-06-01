b89e6d3df636b35f3e615c8bc51ad8bbd5bab84a
stepanvalic <valicekstepan8@seznam.cz>
2024-06-01 16:40:41 +0200

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const themeToggle = document.getElementById("theme-toggle");
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleSwitch = document.querySelector('.input');
+    const currentTheme = localStorage.getItem('theme') || 'light';
 
-    themeToggle.addEventListener("click", function() {
-        document.body.classList.toggle("dark-theme");
-        localStorage.setItem("theme", document.body.classList.contains("dark-theme") ? "dark" : "light");
-    });
-
-    if (localStorage.getItem("theme") === "dark") {
-        document.body.classList.add("dark-theme");
+    if (currentTheme === 'dark') {
+        document.body.classList.add('dark-theme');
+        toggleSwitch.checked = true;
     }
+
+    toggleSwitch.addEventListener('change', function () {
+        if (toggleSwitch.checked) {
+            document.body.classList.add('dark-theme');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.body.classList.remove('dark-theme');
+            localStorage.setItem('theme', 'light');
+        }
+    });
 });
